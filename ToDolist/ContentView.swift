0ac33eca75423b878423319d396b83c27b311f90
armaan535kh <kh.armaan535@gmail.com>
2024-08-14 19:29:06 -0700

    Initial Commit

---
+++ ToDolist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDolist
+//
+//  Created by Armaan Khan  on 14/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDolist/ToDolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDolistApp.swift
+//  ToDolist
+//
+//  Created by Armaan Khan  on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
